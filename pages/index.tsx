d3579680166a369376bf5855578b373b44fb3999
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-11-13 20:10:31 -0500

    Adding colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,27 +12,37 @@ const About: NextPage = () => {
         <Text>Writing articles on TypeScript and JavaScript!</Text>
       </Section>
       <Section title="About Me">
-        <Text mb="5px" maxW="600px">
+        <Text mb="10px" maxW="600px">
           I am Jaime Abbariao. Currently working as a software engineer at{' '}
-          <Link href="https://getbento.com" isExternal>
+          <Link href="https://getbento.com" color="#8FBCBB" isExternal>
             BentoBox
           </Link>
           .
         </Text>
-        <Text mb="5px" maxW="600px">
+        <Text mb="10px" maxW="600px">
           I spent undergrad and grad school figuring out how machine learning
           works, but in the end, I decided I wanted to build and scale
           applications instead.
         </Text>
-        <Text mb="5px" maxW="600px">
+        <Text mb="10px" maxW="600px">
           These days, you can find me working on side-projects in TypeScript or
           tinkering with my Neovim configuration for the millionth time.
+        </Text>
+        <Text maxW="600px">
+          I also take photos. You can find my work on{' '}
+          <Link
+            href="https://www.instagram.com/jaimeabbariao/"
+            color="#8FBCBB"
+            isExternal
+          >
+            Instagram
+          </Link>
         </Text>
       </Section>
       <Section title="Contact">
         <Text maxW="600px">
           Feel free to contact me at{' '}
-          <Link href="mailto:jaimeabbariao@gmail.com">
+          <Link href="mailto:jaimeabbariao@gmail.com" color="#8FBCBB">
             jaimeabbariao@gmail.com
           </Link>{' '}
           for any business inquiries
