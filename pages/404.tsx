809e980fca2857eae2864c57236f6b5165a9db02
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-11-14 14:31:16 -0500

    Adding custom 404

---
+++ pages/404.tsx
@@ -0,0 +1,16 @@
+import { Text } from '@chakra-ui/react'
+
+import PageContainer from '../components/PageContainer'
+import Section from '../components/Section'
+
+function Custom404() {
+  return (
+    <PageContainer>
+      <Section title="404 - Page Not Found">
+        <Text>Interesting. But no.</Text>
+      </Section>
+    </PageContainer>
+  )
+}
+
+export default Custom404
