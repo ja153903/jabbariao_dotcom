5e2761701e70bb08bebb403f54ff89b9cfd939db
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-11-13 20:02:19 -0500

    Adding more to about me

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,25 +9,33 @@ const About: NextPage = () => {
   return (
     <PageContainer>
       <Section title="What am I currently working on?">
-        <Text>
-          Doing a deep dive into Next.js and Chakra UI with TypeScript
-        </Text>
+        <Text>Writing articles on TypeScript and JavaScript!</Text>
       </Section>
       <Section title="About Me">
-        <Text>
+        <Text mb="5px" maxW="600px">
           I am Jaime Abbariao. Currently working as a software engineer at{' '}
           <Link href="https://getbento.com" isExternal>
             BentoBox
           </Link>
           .
         </Text>
+        <Text mb="5px" maxW="600px">
+          I spent undergrad and grad school figuring out how machine learning
+          works, but in the end, I decided I wanted to build and scale
+          applications instead.
+        </Text>
+        <Text mb="5px" maxW="600px">
+          These days, you can find me working on side-projects in TypeScript or
+          tinkering with my Neovim configuration for the millionth time.
+        </Text>
       </Section>
       <Section title="Contact">
-        <Text>
+        <Text maxW="600px">
           Feel free to contact me at{' '}
           <Link href="mailto:jaimeabbariao@gmail.com">
             jaimeabbariao@gmail.com
-          </Link>
+          </Link>{' '}
+          for any business inquiries
         </Text>
       </Section>
     </PageContainer>
