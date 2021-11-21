fc420e56b49dec52e4ab396adf6cc910d552af4d
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-11-21 01:31:51 -0500

    changing maxW

--- components/PageContainer.tsx
+++ components/PageContainer.tsx
@@ -3,7 +3,7 @@ import { Container } from '@chakra-ui/react'
 import type { PageContainerProps } from '../@types'
 
 function PageContainer({ children }: PageContainerProps) {
-  return <Container maxW="lg">{children}</Container>
+  return <Container maxW="2xl">{children}</Container>
 }
 
 export default PageContainer
