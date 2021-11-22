04cebcc1f15b82b072cbc0a7db4bd6c87b865b7c
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-11-21 20:39:15 -0500

    updating this

--- components/PageContainer.tsx
+++ components/PageContainer.tsx
@@ -3,7 +3,7 @@ import { Container } from '@chakra-ui/react'
 import type { PageContainerProps } from '../@types'
 
 function PageContainer({ children }: PageContainerProps) {
-  return <Container minW="container.lg">{children}</Container>
+  return <Container maxW="container.lg">{children}</Container>
 }
 
 export default PageContainer
