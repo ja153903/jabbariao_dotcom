5ff2668fb68e97f9fa5fabcc66b0f5e8e153a400
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-12-04 13:06:42 -0500

    adding script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import '@fontsource/fira-code'
 import '@fontsource/roboto-mono'
 
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 import { ChakraProvider, Container, Flex } from '@chakra-ui/react'
 
 import Introduction from '../containers/Introduction'
@@ -12,15 +13,22 @@ import theme from '../themes'
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Container paddingTop="20px" maxW="container.xl">
-        <Flex align="flex-start" justify="flex-start">
-          <Introduction />
-          <Component {...pageProps} />
-        </Flex>
-        <ScrollToTopButton />
-      </Container>
-    </ChakraProvider>
+    <>
+      <Script
+        async
+        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE}`}
+        crossOrigin="anonymous"
+      />
+      <ChakraProvider theme={theme}>
+        <Container paddingTop="20px" maxW="container.xl">
+          <Flex align="flex-start" justify="flex-start">
+            <Introduction />
+            <Component {...pageProps} />
+          </Flex>
+          <ScrollToTopButton />
+        </Container>
+      </ChakraProvider>
+    </>
   )
 }
 
